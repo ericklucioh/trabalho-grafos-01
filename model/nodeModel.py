2a95e7b165a1cebc32f29ddbfdae5e08ebdcf472
ericklucioh <erickrealoficial@gmail.com>
2025-04-15 19:52:09 -0300

    rename nodemodel

--- model/nodeModel.py
+++ model/nodeModel.py
@@ -5,7 +5,7 @@ class NodeType(Enum):
     MOVIE = "Movie"
 
 
-class Node :
+class NodeModel :
     def __init__(self, node_id: int, node_type: NodeType, name: str):
         self.node_id = node_id
         self.node_type = node_type
