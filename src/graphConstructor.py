24a01cb9fae47f8c722378b62b0251c3a3f466ae
Érick Lúcio <104475806+ericklucioh@users.noreply.github.com>
2025-04-22 21:20:57 -0300

    update graph constructor

--- src/graphConstructor.py
+++ src/graphConstructor.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import json
 import os
 from model.movieModel import MovieModel
@@ -23,6 +22,3 @@ def GraphConstructor():
 
 if __name__ == "__main__":
     main()
-=======
-class MovieModel:
->>>>>>> 89c3c47994aab93d3644e6953f935942be77924a
