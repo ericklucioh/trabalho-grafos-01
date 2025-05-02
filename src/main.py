34861b2a2ea9761f1fdb2c4b4d25a604b5062af7
--global <erickrealoficial@gmai.com>
2025-05-02 16:34:34 -0300

    update main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# src/main.py
-from src.graphConstructor import GraphConstructor
+
+from graphConstructor import GraphConstructor
+
 
 def main():
     # Sua lógica principal
     print(f"Projeto rodando...")
     GraphConstructor()
+
+if __name__ == "__main__":
+    main()
