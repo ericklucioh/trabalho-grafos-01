9146077503ab9ac39d785c54f21f765ca1dc26d3
--global <erickrealoficial@gmai.com>
2025-05-02 16:33:34 -0300

    remove unused archives

--- index.py
+++
@@ -1,7 +0,0 @@
-# index.py
-
-# Aqui você faz o básico e chama o main.py dentro de src
-from src.main import main
-
-if __name__ == "__main__":
-    main()  # Chama a função main que está no src/main.py
