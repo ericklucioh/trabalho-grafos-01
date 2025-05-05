d7722200a8d4a78c3fe8e8749cf587217b385932
--global <erickrealoficial@gmai.com>
2025-05-05 13:39:45 -0300

    update import

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 
 from graphConstructor import GraphConstructor
-from meu_layout import iniciar_interface
+from src.layout import iniciar_interface
 from models.graphModel import Graph
 
 
@@ -9,8 +9,6 @@ def main():
     print(f"Projeto rodando...")
     GraphConstructor()
     graph = Graph()
-    var = graph.find_shortest_path("Brad Pitt", "Angelina Jolie")
-    print(var)
     with open("grafo.txt", "w", encoding="utf-8") as f:
         for node, neighbors in graph.adjacency_list.items():
             f.write(f"{node.name} ({node.node_type.value}) -> ")
